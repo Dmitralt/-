053be408085de8952778b9f1027d94cb8e49e43f
Dmitralt <47825098+Dmitralt@users.noreply.github.com>
2019-04-23 13:35:10 +0300

    Update testingrobot.js

--- testingrobot.js
+++ testingrobot.js
@@ -1,15 +1,17 @@
 //Пользователь вводит координаты робота вначале и куда робот может попасть. из позиции (Х,У) робот может перейти либо в (Х+У,У) либо в (Х,У+Х).   Ороеделить попадет ли робот в конечную точку
 var readline = require('readline-sync');
-/*var X1 = readline.question("What is X1?");
-var Y1 = readline.question("What is Y1?");
-var X2 = readline.question("What is X2?");
-var Y2 = readline.question("What is Y2?");*/
-var X1 = 3;
+var X1 = Number(readline.question("What is X1?"));
+var Y1 = Number(readline.question("What is Y1?"));
+var X2 = Number(readline.question("What is X2?"));
+var Y2 = Number(readline.question("What is Y2?"));
+/*var X1 = 3;
 var Y1 = 2;
 var X2 = 111;
-var Y2 = 157;
+var Y2 = 157;*/
+
+
 console.log("X1=" + X1 + "\nY1=" + Y1 + "\nX2=" + X2 + "\nY2=" + Y2);
-var b = true;
+
 while (X2 > X1 || Y2 > Y1) { if (X2 > Y2) {
             X2 = X2 - Y2;
         }
